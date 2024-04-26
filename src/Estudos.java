c2c27a02f7101b41299b344a6f46a36c4a55a842
venancio2000 <jbrunogvenancio@gmail.com>
2024-04-26 20:06:27 -0300

    first commit

---
+++ src/Estudos.java
@@ -0,0 +1,6 @@
+public class Estudos {
+    public static void main(String[] args){
+        System.out.println("JVM (Java Virtual Machine) - é a Maquina Virtual do Java responsavel por executar o bytecode");
+        System.out.println("JRE (Java Runtime Environment) - Ambiente de execução do java que fornece as bibliotecas padrões do Java para o JDK compilar o seu codigo e para JVM executar o seuprograma ");
+    }
+}
