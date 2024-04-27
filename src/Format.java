46563f15645375849f2ef2cdce550d6c1afc1590
venancio2000 <jbrunogvenancio@gmail.com>
2024-04-26 22:10:15 -0300

    format e casting

---
+++ src/Format.java
@@ -0,0 +1,17 @@
+public class Format {
+    public static void main(String[] args){
+        String nome = "bruno";
+        int idade = 29;
+        double valor = 55.999;
+        System.out.println(String.format("meu nome é %s, eu tenho %d anos, e hoje eu gastei %.2f reais", nome, idade, valor));
+
+        String nomes = "João";
+        int aulas = 4;
+        String mensagem ="""
+                          Olá, %s!
+                          Boas vindas as curso de java.
+                          Teremos %d audas para te mostrar o que é preciso para você dar mergulho na linguagem Java!
+                          """.formatted(nomes, aulas);
+        System.out.println(mensagem);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("Esse e o Screen Match");
+        System.out.println("Filme: Top Gun: maverick");
 
-        System.out.printf("Alõ Mundo!☺\n");
-        System.out.printf("Hello and welcome!");
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento " + anoDeLancamento);
+        boolean icluidoNoPlano = true;
+        double notaFilme = 8.1;
+        //Média calculada pelas as 3 notas da jack, paulo e bruno
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
 
+        String sinopse;
+        sinopse = """
+                 Filme top Gun
+                 Filme de aventura com galã dos anos 80
+                 Muito bom!
+                 ano de lançamento
+                 """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
     }
 }
