d55b7fae2fdb0da25ba16c3018011005c386c69b
venancio2000 <jbrunogvenancio@gmail.com>
2024-04-28 23:36:08 -0300

    Calculo e atribuicao de variavel

---
+++ src/ComparacaoString.java
@@ -0,0 +1,12 @@
+public class ComparacaoString {
+    public static void main(String[] args){
+
+        String senha = "123456";
+
+        if(senha.equals("123456")){
+            System.out.println("Senha correta!");
+        }else{
+            System.out.println("Senha icorreta");
+        }
+    }
+}
---
+++ src/MediaNotas.java
@@ -0,0 +1,14 @@
+public class MediaNotas {
+    public static void main(String[] args){
+
+        double nota1 = 5.0;
+        double nota2 = 6.1;
+        double media = (nota1 + nota2) /2;
+
+        System.out.println("Media da nota do Bruno " + media);
+
+        int classificacao = (int) media;
+        System.out.println("Media Int do alino" + classificacao);
+
+    }
+}
