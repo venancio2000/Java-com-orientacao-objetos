17c5bd590181c6e923acec09f344bbb5b6a96d33
venancio2000 <jbrunogvenancio@gmail.com>
2024-05-04 22:09:31 -0300

    final do projeto  concluido

--- src/BancoBank.java
+++ src/BancoBank.java
@@ -2,53 +2,50 @@ import java.util.Scanner;
 
 public class BancoBank {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
+        String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
 
-        String nome = "Bruno Venancio";
-        String tipoConta = "corrente";
-        double saldo = 2500.00;
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
 
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                
+                """;
+        Scanner leitura = new Scanner(System.in);
 
-        String mensagem ="""
-                          *****************************************
-                          Dados iniciais do cliente:
-                          
-                          Nome do cliente:  %s
-                          Tipo conta:       %s
-                          Saldo inicial:    %.2f
-                          *****************************************
-                          """.formatted(nome, tipoConta, saldo);
-        System.out.println(mensagem);
-        System.out.println("Operações");
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
 
-        String operacoes = """
-                           1- Consultar Saldos
-                           2- Transferir valor
-                           3- Receber Valor 
-                           4- Sair
-                           """;
-        System.out.println(operacoes);
-        System.out.println("Digite a opção desejada: ");
-        int opcao = leitor.nextInt();
-
-        if (opcao == 1){
-            System.out.println("saldo da Conta: " + saldo);
-        } else if (opcao == 2) {
-            System.out.println("Qual valor que deseja transferir");
-            double valor = leitor.nextDouble();
-            if (valor > saldo){
-                System.out.println("Não há saldo para realizar a tranferencia");
-            }else{
-                saldo -= valor;
-                System.out.println("novo saldo " + saldo);
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Não há saldo para realizar a transferência.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
             }
-        } else if (opcao == 3) {
-            System.out.println("Qual valor recebido: ");
-            double valor = leitor.nextDouble();
-            saldo += valor;
-            System.out.println("Seu novo valor " + saldo);
-        }else if (opcao == 4 || opcao != 4){
-            System.out.println("voce saiu do sistema");
         }
     }
 }
