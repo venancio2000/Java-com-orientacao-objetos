8196d364e0be01fc2be80fbe82b73be1a8f8fe00
venancio2000 <jbrunogvenancio@gmail.com>
2024-06-17 22:13:13 -0300

    Calucula idade do veiculo melhoria

--- src/br/com/calculadora/Principal.java
+++ src/br/com/calculadora/Principal.java
@@ -1,11 +1,18 @@
 package br.com.calculadora;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
         Carro meuCarro = new Carro();
-        meuCarro.modelo = "Fiesta";
-        meuCarro.ano = 2015;
-        meuCarro.cor = "Preto";
+
+        System.out.println("Qual seria o modelo do seu veiculo: ");
+        meuCarro.modelo = leitura.nextLine();
+        System.out.println("Qual seria o Ano do seu veiculo: ");
+        meuCarro.ano = leitura.nextInt();
+        System.out.println("Qual seria a cor do seu veiculo: ");
+        meuCarro.cor = leitura.nextLine();
 
         meuCarro.exibeFichaTecnica();
 
